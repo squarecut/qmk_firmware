76b553c2f3b5bdff5d0f77962d80298642e11cc5
Abhishek Malavalli <abhishek.malavalli@fphcare.co.nz>
2024-05-02 16:46:23 +1200

    6x3 corne miryoku layout

--- keyboards/crkbd/keymaps/squarecut_miryoku_custom/keymap.c
+++ keyboards/crkbd/keymaps/squarecut_miryoku_custom/keymap.c
@@ -28,53 +28,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,             KC_H,LSFT_T(KC_J),LCTL_T(KC_K),LALT_T(KC_L), LGUI_T(KC_SCLN), RSFT_T(KC_QUOT),
+      KC_LSFT,LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,             KC_H,LSFT_T(KC_J),LCTL_T(KC_K),LALT_T(KC_L), LGUI_T(KC_QUOT, RSFT_T(KC_QUOT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_Z,   RALT_T(KC_X),KC_C,KC_V,    KC_B,                          KC_N,    KC_M, KC_COMM,  RALT_T(KC_DOT), KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           LT(MO(3),KC_DEL),  MO(1),   LT(MO(2),KC_TAB),     LT(MO(5),KC_ENT),    LT(MO(4),KC_SPC), LT(MO(6),KC_SPC)
+            LT(MO(3),KC_ESC),  LT(MO(1),KC_BSPC),   LT(MO(2),KC_DEL),     LT(MO(5),KC_ENT),    LT(MO(4),KC_SPC), LT(MO(6),KC_TAB)
                                       //`--------------------------'  `--------------------------'
 
   ),
 
     [1] = LAYOUT_split_3x6_3(
-    KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		KC_AGIN, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_NO, 
-	KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, 	KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, 
-	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, 
-	KC_NO, KC_NO, KC_NO, 								KC_ENT,    KC_SPC, KC_SPC
+    KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		    KC_AGIN, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_NO, 
+	KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, 	  KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, 
+	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		      KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, 
+	                  KC_NO, KC_BSPC,   KC_DEL, 		      KC_ENT,    KC_NO, KC_NO
   ),
 
     [2] = LAYOUT_split_3x6_3(
-	KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-	KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, 	KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, 
-	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, 
-	KC_NO, KC_NO, KC_NO, 								KC_BTN2, KC_BTN1, KC_BTN3
+	KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+	KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, 	  KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, 
+	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		      KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, 
+	                      KC_NO, KC_NO, KC_NO, 			      KC_BTN1, KC_BTN3, KC_BTN2
   ),
  
     [3] = LAYOUT_split_3x6_3(
-	KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, 
+	KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, 
 	KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, 	KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, 
-	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-	KC_NO, KC_NO, KC_NO, 								KC_MSTP, KC_MPLY, KC_MUTE
+	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+	                    KC_NO, KC_NO, KC_NO, 				    KC_MSTP, KC_MPLY, KC_MUTE
   ),
     [4] = LAYOUT_split_3x6_3(
   	KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC, 	        KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, 
-	KC_NO, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL, 		    KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, 
-	KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, 		    KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, 
-	KC_DOT, KC_0, KC_MINS, 							    KC_NO, KC_NO, KC_NO
+	KC_NO, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL, 		        KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, 
+	KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, 		        KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, 
+	                  KC_DOT, KC_0, KC_MINS, 				      KC_NO, KC_NO, KC_NO
   ),
     [5] = LAYOUT_split_3x6_3(
 	KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, 		KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, 
 	KC_NO, KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, 			KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, 
 	KC_NO, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE, 			KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, 
-	KC_LPRN, KC_RPRN, KC_UNDS, 									KC_NO, KC_NO, KC_NO
+	              KC_LPRN, KC_RPRN, KC_UNDS, 				        KC_NO, KC_NO, KC_NO
   ),
 
 	[6] = LAYOUT_split_3x6_3(
-	KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, 		KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, 
+	KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, 		  KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, 
 	KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_NO, 		    KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, 
-	KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, 		KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, 
-	KC_APP, KC_SPC, KC_TAB, 							KC_NO, KC_NO, KC_NO
+	KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, 		  KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, 
+	                KC_APP, KC_SPC, KC_TAB, 				  KC_NO, KC_SPC, KC_TAB
   )
 
 
