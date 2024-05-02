b5c4b3ab6092114269ca471f5b5a2703f68f41cb
Abhishek Malavalli <abhishek.malavalli@fphcare.co.nz>
2024-05-02 14:49:03 +1200

    Miryoku with 6 column

---
+++ keyboards/crkbd/keymaps/squarecut_miryoku_custom/keymap.c
@@ -0,0 +1,92 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#define KC_AGIN LCTL(KC_Y)
+#define KC_UNDO LCTL(KC_Z)
+#define KC_CUT LCTL(KC_X) 
+#define KC_COPY LCTL(KC_C) 
+#define KC_PSTE LCTL(KC_V)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT,LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,             KC_H,LSFT_T(KC_J),LCTL_T(KC_K),LALT_T(KC_L), LGUI_T(KC_SCLN), RSFT_T(KC_QUOT),
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,    KC_Z,   RALT_T(KC_X),KC_C,KC_V,    KC_B,                          KC_N,    KC_M, KC_COMM,  RALT_T(KC_DOT), KC_SLSH,  KC_ESC,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           LT(MO(3),KC_DEL),  MO(1),   LT(MO(2),KC_TAB),     LT(MO(5),KC_ENT),    LT(MO(4),KC_SPC), LT(MO(6),KC_SPC)
+                                      //`--------------------------'  `--------------------------'
+
+  ),
+
+    [1] = LAYOUT_split_3x6_3(
+    KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		KC_AGIN, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_NO, 
+	KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, 	KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, 
+	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, 
+	KC_NO, KC_NO, KC_NO, 								KC_ENT,    KC_SPC, KC_SPC
+  ),
+
+    [2] = LAYOUT_split_3x6_3(
+	KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+	KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, 	KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, 
+	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, 
+	KC_NO, KC_NO, KC_NO, 								KC_BTN2, KC_BTN1, KC_BTN3
+  ),
+ 
+    [3] = LAYOUT_split_3x6_3(
+	KC_NO, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, 		RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO, 
+	KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, 	KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, 
+	KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+	KC_NO, KC_NO, KC_NO, 								KC_MSTP, KC_MPLY, KC_MUTE
+  ),
+    [4] = LAYOUT_split_3x6_3(
+  	KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC, 	        KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, 
+	KC_NO, KC_SCLN, KC_4, KC_5, KC_6, KC_EQL, 		    KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, 
+	KC_NO, KC_GRV, KC_1, KC_2, KC_3, KC_BSLS, 		    KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, 
+	KC_DOT, KC_0, KC_MINS, 							    KC_NO, KC_NO, KC_NO
+  ),
+    [5] = LAYOUT_split_3x6_3(
+	KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, 		KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, 
+	KC_NO, KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, 			KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, 
+	KC_NO, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE, 			KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, 
+	KC_LPRN, KC_RPRN, KC_UNDS, 									KC_NO, KC_NO, KC_NO
+  ),
+
+	[6] = LAYOUT_split_3x6_3(
+	KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, 		KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT, KC_NO, 
+	KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_NO, 		    KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_NO, 
+	KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, 		KC_NO, KC_NO, KC_NO, KC_RALT, KC_NO, KC_NO, 
+	KC_APP, KC_SPC, KC_TAB, 							KC_NO, KC_NO, KC_NO
+  )
+
+
+};
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case RGUI_T(KC_SCLN): case RSFT_T(KC_QUOT):
+            return TAPPING_TERM + 200; //- 100;     
+        default:
+            return TAPPING_TERM + 200;
+            
+    }
+}
+
